9605294a6a7920ae49118eae482321c546fe8726
erik-larkin <erik.larkin789@gmail.com>
2022-12-05 00:32:34 +0000

    added rucksack prioritiser

---
+++ solutions/src/RucksackPrioritiser.java
@@ -0,0 +1,19 @@
+public class RucksackPrioritiser extends AbstractSolution 
+{
+	String[] rucksacks;
+	public RucksackPrioritiser()
+	{
+		super("./inputs/rucksacks");
+		rucksacks = super.lines().toArray(String[]::new);
+	}
+
+	public int solutionOne()
+	{
+		return 0;
+	}
+
+	public int solutionTwo()
+	{
+		return 0;
+	}
+}
